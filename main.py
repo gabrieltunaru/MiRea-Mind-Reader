8c55c259d0e3f904b07d12dcdb38fbf726a3ae9e
Gabriel Tunaru <gabi.tunaru24@gmail.com>
2021-03-24 22:12:04 +0200

    semi-working ui

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+import PySimpleGUI as sg
 
 
 def get_knowledge_base():
@@ -19,24 +20,29 @@ def can_ask_this_question(required_flags, flags):
 
 
 def ask(conclusions, questions, flags):
+    layout = [[sg.Text("Press START to start", key='question', size=(50, 2))], [
+        sg.Button("YES"), sg.Button("NO")], [sg.Text("Result", key='result', size=(50, 2))]]
+    window = sg.Window(title="Demo", layout=layout,
+                       margins=(100, 100), finalize=True)
     partial_conclusion = conclusions
     remaining_questions = dict()
     current_flags = flags
-    for flag in questions:
-        if not can_ask_this_question(questions[flag]['required'], flags):
-            remaining_questions[flag] = questions[flag]
-            continue
-        res = input(questions[flag]['text'] + " ")
-        include = res == 'y'
-        current_flags.add(flag) if include else current_flags
-        partial_conclusion = list(filter(lambda x: not (
-            (flag in x['flags']) ^ include), partial_conclusion))
-        remaining_conclusions = len(partial_conclusion)
-        if remaining_conclusions == 0:
-            return "Not found"
-        elif remaining_conclusions == 1:
-            return partial_conclusion[0]['name']
-    return ask(partial_conclusion, remaining_questions, current_flags)
+    while len(partial_conclusion) > 0:
+        for flag in questions:
+            if not can_ask_this_question(questions[flag]['required'], flags):
+                remaining_questions[flag] = questions[flag]
+                continue
+            window['question'].update(questions[flag]['text'])
+            event, _ = window.read()
+            include = event == 'YES'
+            current_flags.add(flag) if include else current_flags
+            partial_conclusion = list(filter(lambda x: not (
+                (flag in x['flags']) ^ include), partial_conclusion))
+            remaining_conclusions = len(partial_conclusion)
+            if remaining_conclusions == 0:
+                return "Not found"
+            elif remaining_conclusions == 1:
+                return partial_conclusion[0]['name']
 
 
 def start():
